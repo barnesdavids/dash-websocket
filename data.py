454ba6c8d6b3c4fda0bdfc18e6b13353898a7434
barnesdavids <107556927+barnesdavids@users.noreply.github.com>
2023-10-06 14:37:57 +0200

    database connection and db created

--- data.py
+++ data.py
@@ -4,3 +4,20 @@ import aiosqlite
 import sqlite3
 import json
 
+conn = sqlite3.connect('./data.db')
+
+cursor = conn.cursor()
+
+cursor.execute(" DROP TABLE IF EXISTS trades ")
+cursor.execute(""" CREATE TABLE trades(
+                id int PRIMARY KEY,
+                time int,
+                quantity int,
+               price float)""")
+
+cursor.execute("CREATE INDEX index_time ON trades(time)")
+
+conn.commit()
+
+conn.close()
+               
