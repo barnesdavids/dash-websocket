887e4caff0ecf956a5477c2c9b9964024f1dc233
barnesdavids <107556927+barnesdavids@users.noreply.github.com>
2023-10-06 15:46:37 +0200

    data downloaded

--- data.py
+++ data.py
@@ -1,10 +1,11 @@
+from websockets import connect
 import asyncio
 import sys
 import aiosqlite
 import sqlite3
 import json
 
-conn = sqlite3.connect('./data.db')
+conn = sqlite3.connect("./data.db")
 
 cursor = conn.cursor()
 
@@ -20,4 +21,34 @@ cursor.execute("CREATE INDEX index_time ON trades(time)")
 conn.commit()
 
 conn.close()
+
+url = "wss://stream.binance.com:9443/ws/btcusdt@aggTrade"
+
+async def save_down(url):
+
+    async with connect(url) as websocket:
+
+        trades_buffer =[] ### create an empty list to append each trade
+
+        while True:
+            data = await websocket.recv()
+            data = json.loads(data) ### binance sends data as str object - hence convert to dictionary
+            trades_buffer.append((data['a'], data['T'], data['q'], data['p'])) ### pass in as tuple
+
+            print(data)
+
+            if len(trades_buffer) > 10:
+                
+                print("Writing to DB")
+
+                async with aiosqlite.connect("./data.db") as db:
+
+                    await db.executemany(""" INSERT INTO trades(id, time, quantity, price) 
+                                         VALUES(?,?,?,?)""", trades_buffer)
+                    await db.commit()
+
+                trades_buffer = []
+            
+
+asyncio.run(save_down(url))
                
